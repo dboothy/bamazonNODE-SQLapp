7ed9ca89ba921d87be1221bdf2208dfdbae20d95
David Booth-Metwally <davidbmetwally@gmail.com>
2018-02-26 23:48:12 -0800

    commented js app file.

--- bamazon.js
+++ bamazon.js
@@ -1,12 +1,15 @@
 var mysql = require("mysql");
+//npm mysql installation required
 var inquirer = require("inquirer");
+//npm inquirer installation required
 var Table = require("cli-table");
-var colors = require("colors")
+//npm cli-table installation required
 var code = require("./key.js")
-
+//added a file to be gitignored to mask password
 
 
 var connection = mysql.createConnection({
+//initializes connection to mysql via local server
   host: "localhost",
   port: 3306,
 
@@ -21,25 +24,27 @@ var connection = mysql.createConnection({
 
 
 connection.connect(function(err){
+//inidicates the connection upon launch of app
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId + "\n")
 });
 readTable();
 
 function readTable(){
+//function reads table called "products" in mysql database
 	connection.query("SELECT * FROM products", function (err, res) {
 		if(err) console.log(err)
-
+//creates a table structure and inserts column name strings
 		var table = new Table({
 				head: ["id", "Product Name", "Department Name", "Price ($)", "Quantity in Stock" ]
 		})
-
+//loop through the connection parameter that was passed in to retrieve all of the data within the database
 		for (var i = 0; i < res.length; i ++){
 			table.push(
 				[res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_q]
 					)
 		}
-		
+//sends the data to the generated table that was invoked on line 45
 		console.log(table.toString());
 		purchaseItem();
 		})
@@ -47,31 +52,38 @@ function readTable(){
 }
 
 	function purchaseItem(){
+//retrieves data from table "products"
 
 	connection.query("SELECT * FROM products", function(err, results) {
 		if (err) throw err;
-
+//inquirer prompt to collect user input
 	inquirer
 		.prompt([
 			{
 				name: "list",
 				type: "rawlist",
 				choices: function() {
+				//choices function generates a list of input selections for user to  choose from locked in
+				//within an array. 
 					var choiceArray = [];
 					for (var i = 0; i < results.length; i++) {
 						choiceArray.push(results[i].product_name);
+					//loops through all of the data within the database table
 					}
+					//returns the results from database gather within the array to be selected through choices
 					return choiceArray;
 				},
 				message: "\nWhat is the ID of the item you would like to purchase?\n"
 			},
 			{
+			//prompt collects user input
 				name:"purchase",
 				type:"input",
 				message: "How many would like to purchase?"
 			}
 		])
 		.then(function(response){
+		//. then loops through the response from the database
 
 			var chosenItem;
 			for (var i = 0; i < results.length; i++){
@@ -79,20 +91,25 @@ function readTable(){
 					chosenItem = results[i];
 				}
 			}
-			console.log(chosenItem)
-			// if (chosenItem.stock_q)
+			
+			/*condition determines that if the amount input is greater than the stock amount,
+			then the user's request for a number above the amount available will not be depleted */
 
 
 			if (chosenItem.stock_q >= parseInt(response.purchase)){	
+			// variable "changestock" assigned to represent the amount left in stock after input request
 			var changeStock = chosenItem.stock_q - parseInt(response.purchase)		
 			connection.query(
 				"UPDATE products SET ? WHERE ?",
 				[
+				//SQL query on where and which data to update on the table 
 					{
 						stock_q: changeStock
 					},
+					//reduces the stock by the input of the user for their request
 					{
 						id: chosenItem.id
+						//determines which row will be affected on the basis of the user id
 					}
 				],
 			function(error) {
@@ -100,10 +117,12 @@ function readTable(){
 			console.log("Item purchased")
 			console.log("You paid $" + chosenItem.price * response.purchase + "."+ " Thanks!")
 			readTable();
+			//when the user purchases an amount of an item, the amount paid is shown
 
 			}
 		);
 	}
+	//message for when there is less stock than the request for quantity from the user.
 	else{
 		console.log("Not enough stock!")
 		readTable()
