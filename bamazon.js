d4f0f08e90e408af1b653dfb284f6454447d8083
David Booth-Metwally <davidbmetwally@gmail.com>
2018-02-26 19:06:56 -0800

    added the cost to the purchase

--- bamazon.js
+++ bamazon.js
@@ -98,7 +98,9 @@ function readTable(){
 			function(error) {
 			if(error) throw err;
 			console.log("Item purchased")
+			console.log("You paid $" + chosenItem.price * response.purchase + "."+ " Thanks!")
 			readTable();
+
 			}
 		);
 	}
@@ -112,119 +114,6 @@ function readTable(){
 
 
 		
-		
-	
-
-			// var input = response.purchase
-			// var inputParsed = parseInt(input)
-
-			// if(Number.isNaN(inputParsed)){
-			// 	console.log("Please input a valid number [1-10");
-			// } else if(inputParsed === 0){
-			// 	console.log("Please input a valid number [1-10");
-			// } else if(inputParsed > 10){
-
-			// }else{
-			// 	console.log("Great choice!")
-			// 	purchasedArray.push(inputParsed)
-			// 	console.log(purchasedArray)
-				
-			
-
-
-
-// readTable();
-// start();
-// selectItem();
-
-
-// function start() {
-
-// 	connection.query("SELECT * FROM products", function(err, results){
-// 		if (err) throw err;
-
-// 		inquirer
-// 			.prompt({
-// 				name:"selection",
-// 				type:"input",
-// 				message: "What is the ID of the item you would like to purchase? [Quit with Q]"
-
-
-// 			})
-// 			.then(function(response){
-// 				var input = response.selection
-// 				inputParsed = parseInt(input)
-
-// 				if(Number.isNaN(inputParsed)){
-// 		        console.log("Please input a valid number [1-10]");
-// 		        start();
-// 		    	} else if(inputParsed === 0){
-// 		    		console.log("Please input a valid number [1-10]");
-// 		    		start();
-// 		    	}else{
-// 		    		console.log("Great choice!")
-		    		
-// 		    	} 
-
-// 			});
-// 	});
-// }
-
-
-
-
-
-
-// function selectItem(){
-// 	 connection.query("SELECT * FROM products", function(err, results) { 
-// 	 	if (err) throw err;
-
-// 	 	inquirer
-// 	 		.prompt([
-// 	 		{
-// 	 			name:"choice",
-// 	 			type:"rawlist",
-//  				choices: function () {
-// 				var choiceArray = [];
-// 				for (var i = 0; i < results.length; i++){
-// 					choiceArray.push(results[i].id);
-// 				}
-// 				return choiceArray;
-// 				console.log(choiceArray[0])
-// 				},
-// 				message: "Which item would you like to purchase?"
-// 	 		},
-// 	 		{
-// 	 			name:"quantity",
-// 	 			type: "input",
-// 	 			message: "How many would you like?"
-// 	 		}
-// 	 	])
-// 	 		.then(function(answer){
-// 	 			var chosenItem;
-// 	 			for (var i = 0; i < results.length; i++){
-// 	 				if (results[i].id === answer.choice){
-// 	 					chosenItem = results[i];
-// 	 				}
-// 	 			}
-// 	 			connection.query(
-// 	 				"DELETE products SET ? WHERE ?",
-// 	 				[
-// 	 					{
-// 	 						stock_q: answer.quantity
-// 	 					},
-// 	 					{
-// 	 						id: chosenItem.id
-// 	 					}
-// 	 				],
-// 	 				function(error) {
-// 	 					if (error) throw err;
-// 	 					console.log("Product purchased!");
-// 	 				}
-// 	 				);
-// 	 		})
-// 	 	});
-// 	}
 
 
 	
