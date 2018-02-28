b9c82c19a21645089fe52d0f31b7c19a984231f4
David Booth-Metwally <davidbmetwally@gmail.com>
2018-02-27 21:09:26 -0800

    added quit

--- README.md
+++ README.md
@@ -7,11 +7,8 @@ App built with Node and SQL which simulates the taking of orders from customers,
 
 To get started, below are some instructions for what is needed to get bamazon up and running.
 
-###Prequisites
 
-
-
-###Requirements
+##Requirements
 
 Node must be installed for bamazon to work.
 
@@ -25,26 +22,58 @@ cli-table
 mysql
 inquirer
 
-###Database 
+##Database 
 
-bamazon uses a mysql database and generates the data from a created table.
+bamazon uses mysql database to pull data from a created table.
 
-Use my sql to insert data into 
+Once mySQL and mySQL workbench are installed properly, make a database with columns "id", "name" (of the product), "department name", "price", and "quantity in stock".
+
+Afterwards, insert the appropriate values. 
+
+Then, run the table, save the database and continue to the node .js file for the logic.
+
+
+##Node
+
+####NPMS
+require all NPMs that are needed listed under installation requirements.
+
+
+##mySQL
+
+In node, create a connection to mySQL through a PORT that runs locally on your machine. Insert your mySQL credentials with the database name.
+
+##Table
+
+Create a new table using "New" to insert column strings as the new table's column heads.
+
+Once connected, retrieve table data from the database with the query: SELECT * FROM (tablename) and iterate through the data and then inserting those vales with "toString".
+
+Doing this will generate your table on the command line.
+
+##Inquirer
+
+Perform another connection query with a callback function passing in paramters to retrieve response from the table.
+
+Choose the "rawlist" type prompt and keep track of its "name".
+
+Before adding the message to prompt users for the item ID that a user can choose from, add a choices function. Within th function, interate through the response to pull the name from each item within the table from the mySQL connection query and push to an empty array. After looping through the response name, return the array within choices, and the user selection will include the "name" from the data table response.
+
+The next prompt type will be "input" and will include the message asking the user for the quantity of items.
+
+Within the .then function after the inquirer prompt, interate through the response that was sent by the database in the previous connection query. Within the loop, create a condition for the interation so that input must be the same as th
 
 
 
-<<<<<<< HEAD
+
+
 ![](assets/table.gif)
-=======
-
 
 ![]("assets/bamazongif1")
 
 ![]("assets/bamazongif2")
 
-
 ![]("assets/bamazongif3")
-
 
 ![]("assets/bamazongif4")
 
--- bamazon.js
+++ bamazon.js
@@ -7,6 +7,10 @@ var Table = require("cli-table");
 var code = require("./key.js")
 //added a file to be gitignored to mask password
 
+var table;
+var items;
+
+var chosenItem;
 
 var connection = mysql.createConnection({
 //initializes connection to mysql via local server
@@ -28,75 +32,103 @@ connection.connect(function(err){
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId + "\n")
 });
+
 readTable();
+/*
+promptUserForProduct()
+.then(function() {
+	console.log('QUESTION')
+
+})
+*/
+
+
+function printTable() {
+	console.log(table.toString());
+}
 
 function readTable(){
 //function reads table called "products" in mysql database
 	connection.query("SELECT * FROM products", function (err, res) {
 		if(err) console.log(err)
 //creates a table structure and inserts column name strings
-		var table = new Table({
+		table = new Table({
 				head: ["id", "Product Name", "Department Name", "Price ($)", "Quantity in Stock" ]
 		})
 //loop through the connection parameter that was passed in to retrieve all of the data within the database
 		for (var i = 0; i < res.length; i ++){
 			table.push(
 				[res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_q]
-					)
+			)
 		}
+		items = res
+		printTable()
+		promptUserForProduct()
+			//.then(handleProductAnswer)
+
 //sends the data to the generated table that was invoked on line 45
-		console.log(table.toString());
-		purchaseItem();
-		})
 		
+		// purchaseItem();
+		/*
+		promptUserForProduct()
+			.then(function() {
+				console.log('Asked queston')
+			})
+			.then(handleProductAnswer)
+			.then(handleQuantityAnswer)
+		*/
+	})
 }
 
-	function purchaseItem(){
-//retrieves data from table "products"
-
-	connection.query("SELECT * FROM products", function(err, results) {
-		if (err) throw err;
-//inquirer prompt to collect user input
+function promptUserForProduct() {
 	inquirer
-		.prompt([
-			{
-				name: "list",
-				type: "rawlist",
-				choices: function() {
-				//choices function generates a list of input selections for user to  choose from locked in
-				//within an array. 
-					var choiceArray = [];
-					for (var i = 0; i < results.length; i++) {
-						choiceArray.push(results[i].product_name);
-					//loops through all of the data within the database table
-					}
-					//returns the results from database gather within the array to be selected through choices
-					return choiceArray;
-				},
-				message: "\nWhat is the ID of the item you would like to purchase?\n"
+	.prompt([
+		{
+			name: "list",
+			type: "rawlist",
+			choices: function() {
+			//choices function generates a list of input selections for user to  choose from locked in
+			//within an array. 
+				var choiceArray = [];
+				for (var i = 0; i < items.length; i++) {
+					choiceArray.push(items[i].product_name);
+				//loops through all of the data within the database table
+				}
+				choiceArray.push("Q")
+				//returns the results from database gather within the array to be selected through choices
+				return choiceArray;
 			},
-			{
-			//prompt collects user input
-				name:"purchase",
-				type:"input",
-				message: "How many would like to purchase?"
+			message: "\nWhat is the ID of the item you would like to purchase?(press 11 for exit)\n"
+		}
+	])
+	.then(function(response) {
+		if(response.list === "Q"){
+				console.log("QUIT")
+				connection.end()
+				process.exit()
 			}
-		])
-		.then(function(response){
-		//. then loops through the response from the database
-
-			var chosenItem;
-			for (var i = 0; i < results.length; i++){
-				if (results[i].product_name === response.list){
-					chosenItem = results[i];
+			for (var i = 0; i < items.length; i++){
+				if (items[i].product_name === response.list){
+					chosenItem = items[i];
 				}
 			}
-			
-			/*condition determines that if the amount input is greater than the stock amount,
-			then the user's request for a number above the amount available will not be depleted */
+			promptUserForQuantity()
+		})
+}
 
 
-			if (chosenItem.stock_q >= parseInt(response.purchase)){	
+function promptUserForQuantity() {
+	inquirer
+	.prompt([
+		{
+		//prompt collects user input
+			name:"purchase",
+			type:"input",
+			message: "How many would like to purchase? (Choose number or else will Quit)"
+		}
+	])
+	.then(function(response) {
+		if (chosenItem.stock_q >= parseInt(response.purchase)){	
 			// variable "changestock" assigned to represent the amount left in stock after input request
 			var changeStock = chosenItem.stock_q - parseInt(response.purchase)		
 			connection.query(
@@ -113,24 +145,122 @@ function readTable(){
 					}
 				],
 			function(error) {
-			if(error) throw err;
-			console.log("Item purchased")
-			console.log("You paid $" + chosenItem.price * response.purchase + "."+ " Thanks!")
-			readTable();
-			//when the user purchases an amount of an item, the amount paid is shown
+
+				if(error) throw err;
+				console.log("Item purchased")
+				console.log("You paid $" + chosenItem.price * response.purchase + "."+ " Thanks!")
+				readTable();
+				//when the user purchases an amount of an item, the amount paid is shown
 
 			}
-		);
-	}
+				);
+			}else if(response.purchase == "Q" || "q"){
+				process.exit()
+			}
 	//message for when there is less stock than the request for quantity from the user.
-	else{
-		console.log("Not enough stock!")
-		readTable()
-	}
-})
-	});
-
+		else{
+			console.log("Not enough stock!")
+			return readTable()
+		}
+	})
 }
+
+/*
+
+	function purchaseItem(){
+//retrieves data from table "products"
+
+	connection.query("SELECT * FROM products", function(err, results) {
+		if (err) throw err;
+//inquirer prompt to collect user input
+		])
+}
+*/
+
+
+
+
+
+/****************************************************************************/
+// 	inquirer
+// 		.prompt([
+// 			{
+// 				name: "list",
+// 				type: "rawlist",
+// 				choices: function() {
+// 				//choices function generates a list of input selections for user to  choose from locked in
+// 				//within an array. 
+// 					var choiceArray = [];
+// 					for (var i = 0; i < results.length; i++) {
+// 						choiceArray.push(results[i].product_name);
+// 					//loops through all of the data within the database table
+// 					}
+// 					choiceArray.push("Q")
+// 					//returns the results from database gather within the array to be selected through choices
+// 					return choiceArray;
+// 				},
+// 				message: "\nWhat is the ID of the item you would like to purchase?\n"
+// 			},
+// 			{
+// 			//prompt collects user input
+// 				name:"purchase",
+// 				type:"input",
+// 				message: "How many would like to purchase?"
+// 			}
+// 		])
+// 		.then(function(response){
+// 		//. then loops through the response from the database
+
+// 			var chosenItem;
+
+// 			if(response.list ==="Q"){
+// 				console.log("QUIT")
+// 			}
+// 			for (var i = 0; i < results.length; i++){
+// 				if (results[i].product_name === response.list){
+// 					chosenItem = results[i];
+// 				}
+// 			}
+			
+// 			condition determines that if the amount input is greater than the stock amount,
+// 			then the user's request for a number above the amount available will not be depleted 
+
+
+// 			if (chosenItem.stock_q >= parseInt(response.purchase)){	
+// 			// variable "changestock" assigned to represent the amount left in stock after input request
+// 			var changeStock = chosenItem.stock_q - parseInt(response.purchase)		
+// 			connection.query(
+// 				"UPDATE products SET ? WHERE ?",
+// 				[
+// 				//SQL query on where and which data to update on the table 
+// 					{
+// 						stock_q: changeStock
+// 					},
+// 					//reduces the stock by the input of the user for their request
+// 					{
+// 						id: chosenItem.id
+// 						//determines which row will be affected on the basis of the user id
+// 					}
+// 				],
+// 			function(error) {
+// 			if(error) throw err;
+// 			console.log("Item purchased")
+// 			console.log("You paid $" + chosenItem.price * response.purchase + "."+ " Thanks!")
+// 			readTable();
+// 			//when the user purchases an amount of an item, the amount paid is shown
+
+// 			}
+// 		);
+// 	}
+// 	//message for when there is less stock than the request for quantity from the user.
+// 	else{
+// 		console.log("Not enough stock!")
+// 		readTable()
+// 	}
+// })
+// 	});
+
+// }
 
 
 		
